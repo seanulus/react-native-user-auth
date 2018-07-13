b5f69139e9c2c8e463d92bc06c884496a3e522cc
Sean McDermott <seanjmac@hotmail.com>
2018-07-13 15:28:45 -0700

    Add a text input and fix errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './common';
+import LoginForm from './LoginForm';
 
 
 class App extends Component {
@@ -20,7 +21,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText='Authentication' />
-        <Text>An app</Text>
+        <LoginForm />
       </View>
     );
   }
