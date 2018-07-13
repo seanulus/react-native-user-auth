703fe5500b40804516711de9f26a480a228a2ca4
Sean McDermott <seanjmac@hotmail.com>
2018-07-13 15:09:27 -0700

    Add reusable components to project. Start firebase configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './common';
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyDXvIV7cgEi36U6UXr9QNsVXtc5oFAWXrE',
+    authDomain: 'authentication-87a99.firebaseapp.com',
+    databaseURL: 'https://authentication-87a99.firebaseio.com',
+    projectId: 'authentication-87a99',
+    storageBucket: 'authentication-87a99.appspot.com',
+    messagingSenderId: '869996393035'
+  });
+  }
+
   render() {
     return (
       <View>
+        <Header headerText='Authentication' />
         <Text>An app</Text>
       </View>
     );
