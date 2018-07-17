5817f3899d8f77240b34969fe02c4d194e18aad5
Sean McDermott <seanjmac@hotmail.com>
2018-07-17 10:22:15 -0700

    Add more logic for user authentication. Working on rendering bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './common';
+import { Header, Button, Spinner } from './common';
 import LoginForm from './LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
-    apiKey: 'AIzaSyDXvIV7cgEi36U6UXr9QNsVXtc5oFAWXrE',
-    authDomain: 'authentication-87a99.firebaseapp.com',
-    databaseURL: 'https://authentication-87a99.firebaseio.com',
-    projectId: 'authentication-87a99',
-    storageBucket: 'authentication-87a99.appspot.com',
-    messagingSenderId: '869996393035'
-  });
+      apiKey: 'AIzaSyDXvIV7cgEi36U6UXr9QNsVXtc5oFAWXrE',
+      authDomain: 'authentication-87a99.firebaseapp.com',
+      databaseURL: 'https://authentication-87a99.firebaseio.com',
+      projectId: 'authentication-87a99',
+      storageBucket: 'authentication-87a99.appspot.com',
+      messagingSenderId: '869996393035'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+      <Button>Log Out</Button>
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />
+    }
+  };
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
